02c9122f464b86c25fde4fb0604502bfcb409140
SergeyTester <sergey@antalika.com>
2015-01-05 16:24:31 +0200

    project

---
+++ app/cache/dev/twig/8d/3b/2e5d2e78b718c7934000b2d4c43c85e321039eae0ef2ae9f0f01c4c4d1b6.php
@@ -0,0 +1,31 @@
+<?php
+
+/* BloggerBlogBundle:Page:index.html.twig */
+class __TwigTemplate_8d3b2e5d2e78b718c7934000b2d4c43c85e321039eae0ef2ae9f0f01c4c4d1b6 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "1111111111111";
+    }
+
+    public function getTemplateName()
+    {
+        return "BloggerBlogBundle:Page:index.html.twig";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  19 => 1,);
+    }
+}
---
+++ app/cache/dev/twig/e5/73/a743b3f0814910379c22e0fb550909d7e87b11f228f4d8fee6e80d86b0cb.php
@@ -0,0 +1,39 @@
+<?php
+
+/* BloggerBlogBundle:Default:index.html.twig */
+class __TwigTemplate_e573a743b3f0814910379c22e0fb550909d7e87b11f228f4d8fee6e80d86b0cb extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "Hello ";
+        echo twig_escape_filter($this->env, (isset($context["name"]) ? $context["name"] : $this->getContext($context, "name")), "html", null, true);
+        echo "!
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "BloggerBlogBundle:Default:index.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  19 => 1,);
+    }
+}
